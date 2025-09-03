eadf563d2b6ebb2b9419cd4d84477b4046a21216
KrishnaSaiRadhesh <jksr.0603@gmail.com>
2025-09-03 11:55:37 +0530

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,28 @@
-import express from "express"
-import { ENV } from "./config/env.js"
-import { connectDB } from "./config/db.js"
- 
-const app = express()
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 
-connectDB();
+import userRoutes from "./routes/user.route.js";
 
-app.get("/",(req, res)=> {
-    res.send("Hello from Server.js")
-})
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => res.send("Hello from server"));
+
+app.use("/api/users", userRoutes);
 
 const startServer = async () => {
   try {
     await connectDB();
+
     app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
-    
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
