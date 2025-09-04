3ad1616f0b83801feb31cc83870b6887f1eb2eb6
KrishnaSaiRadhesh <jksr.0603@gmail.com>
2025-09-04 18:13:21 +0530

    tabs setup

---
+++ mobile/app/(tabs)/notifications.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotificationsScreeen = () => {
+  return (
+    <View>
+      <Text>NotificationsScreeen</Text>
+    </View>
+  )
+}
+
+export default NotificationsScreeen
